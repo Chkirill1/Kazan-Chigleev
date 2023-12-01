6c1cca6118103d40f74522c206f1354e59de3de2
Kirill <kirillc549@gmail.com>
2023-12-01 18:47:34 +0300

    mainpy with randomcolor

--- first.py
+++ first.py
@@ -1,6 +1,35 @@
-def main():
-    print("My first git-repo")
+import pygame
+
+try:
+    stroka = input().split()
+    stroka = list(map(lambda x: int(x), stroka))
+    a, n = stroka[0], stroka[1]
+except:
+    print("Неправильный формат!")
 
 
-if __name__ == "__main__":
-    main()
+def draw(screen):
+    screen.fill((255, 255, 255))
+    storona = a // n
+    x = 0
+    yy = 0
+    for i in range(n):
+        if i % 2 == 0:
+            x = 0
+        else:
+            x = storona
+        for y in range(n):
+            pygame.draw.rect(screen, pygame.Color("black"), (x, yy, a // n, a // n))
+            x += storona * 2
+        yy += storona
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = a, a
+    screen = pygame.display.set_mode(size)
+    draw(screen)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from PyQt5.QtCore import QPoint
+from radius import Ui_Form
 import random
 import sys
 
 
-class DZ(QWidget):
+class DZ(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.do_paint = None
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
-        uic.loadUi("radius.ui", self)
         self.create.clicked.connect(self.paint)
 
 
@@ -34,7 +34,7 @@ class DZ(QWidget):
     def draw_circle(self, qp):
         weight = random.randint(10, 170)
         coords = QPoint(300, 250)
-        qp.setBrush(QColor(250, 255, 32))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         qp.drawEllipse(coords, weight, weight)
 
 
